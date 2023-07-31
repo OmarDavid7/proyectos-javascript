b44f888b412ac89359ea69fee64f0baf85df9587
Omar <omar3568@gmail.com>
2023-07-31 17:41:27 -0500

    calculadora en javascript

---
+++ calculadora.js
@@ -0,0 +1,16 @@
+//esto agrega a la pantalla el valor del boton de la calculadora que toquemos
+function agregar(valor){
+    document.getElementById('pantalla').value += valor;
+}
+
+//vacia la pantalla
+function borrar(){
+    document.getElementById('pantalla').value = '';
+}
+
+//toma el valor de la pantalla y ejecuta los calculos
+function calcular(){
+    const valorPantalla = document.getElementById('pantalla').value;
+    const resultado = eval(valorPantalla)
+    document.getElementById('pantalla').value = resultado
+}
